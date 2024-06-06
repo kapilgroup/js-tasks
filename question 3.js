59d0c88fd62c6cde5bd5bd8dfe77f2a57c5bc540
Srinitha <125534861+kapilgroup@users.noreply.github.com>
2024-06-06 11:54:58 +0530

    Update question 3.js

--- question 3.js
+++ question 3.js
@@ -2,7 +2,7 @@ const totalVotes = (...votes) => {
     return votes.reduce((total, vote) => total + vote, 0);
   };
   
-  // Example usage
+
   console.log(totalVotes(5400, 6700, 1500,4000)); 
   console.log(totalVotes(100, 200, 300)); 
   console.log(totalVotes()); 
